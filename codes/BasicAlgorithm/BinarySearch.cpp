77221152ec812153d5ec23e20e0974f9ed991510
hxc <cei196505153@163.com>
2023-03-29 15:17:59 +0800

    完善二分代码

--- codes/BasicAlgorithm/BinarySearch.cpp
+++ codes/BasicAlgorithm/BinarySearch.cpp
@@ -3,8 +3,43 @@
 #include <algorithm>
 using namespace std;
 
+const int N = 2e5 + 10;
+
+int n, a[N];
+
+int Bsearch1(int *a, int l, int r, int x) // the first >= x
+{
+	while(l < r)
+	{
+		int mid = l + r >> 1;
+		if(a[mid] >= x) r = mid;
+		else l = mid + 1;
+	}
+	return l;
+}
+
+int Bsearch2(int *a, int l, int r, int x) 	// the last <= x
+{
+	while(l < r)
+	{
+		int mid = l + r + 1 >> 1;
+		if(a[mid] <= x) l = mid;
+		else r = mid - 1;
+	}
+	return l;
+}
+
 int main()
 {
+	cin >>  n;
+	for(int i = 0; i < n; i ++ ) cin >> a[i];
+	
+	// find the first >= x or > x element
+	int idx = Bsearch1(a, 0, n - 1, x);
+	
+	// find the last <= x or < x element
+	int idx2 = Bsearch2(a, 0, n - 1, x);
+
 
 	return 0;
 }
